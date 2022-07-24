132372facdfb4c37e5a2bcb03fc4aaf590cc7d5c
sosat1101 <air911q@gmail.com>
2022-07-24 18:50:07 +0800

    可以正常与后端服务器通信

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/user/login',
+    url: '/eduService/user/login',
     method: 'post',
     data
   })
@@ -10,7 +10,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/vue-element-admin/user/info',
+    url: '/eduService/user/info',
     method: 'get',
     params: { token }
   })

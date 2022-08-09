bba5d35fe275884af55d1826e2738d9964cfb79c
sosat1101 <air911q@gmail.com>
2022-08-09 22:19:55 +0800

    上传文件

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -28,6 +28,31 @@
         <el-input v-model="teacher.intro" :rows="10" type="textarea" />
       </el-form-item>
       <!-- 讲师头像：TODO -->
+
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar" />
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+        <!--
+v-show：是否显示上传组件
+:key：类似于id，如果一个页面多个图片上传控件，可以做区分
+:url：后台上传的url地址
+@close：关闭上传组件
+@crop-upload-success：上传成功后的回调 -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :key="imagecropperKey"
+          :width="300"
+          :height="300"
+          :url="BASE_API + '/eduOss/fileOss/upload'"
+          field="avatar"
+          @close="close"
+          @crop-upload-success="cropSuccess"
+        />
+      </el-form-item>
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
       </el-form-item>
@@ -37,13 +62,20 @@
 
 <script>
 import teacherApi from '@/api/edu/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 export default {
+  components: { ImageCropper, PanThumb },
   data() {
     return {
       teacher: {
         sort: 0
       },
-      saveBtnDisabled: false
+      saveBtnDisabled: false,
+      imagecropperShow: false,
+      imagecropperKey: 0,
+      BASE_API: process.env.VUE_APP_BASE_API
+
     }
   },
   watch: {
@@ -60,6 +92,14 @@ export default {
     }
   },
   methods: {
+    close() {
+      this.imagecropperShow = false
+    },
+    cropSuccess(data) {
+      this.teacher.avatar = data
+      this.close()
+    },
+
     // 根据讲师id查询方法
     async fetchDataById(id) {
       const { data } = await teacherApi.getTeacherById(id)

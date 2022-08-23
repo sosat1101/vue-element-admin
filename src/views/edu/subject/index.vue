5e7efffccdd671594b9f89ffc810734f2f0041ee
sosat1101 <air911q@gmail.com>
2022-08-23 21:46:41 +0800

    学科

--- src/views/edu/subject/index.vue
+++ src/views/edu/subject/index.vue
@@ -2,61 +2,20 @@
   <div class="app-container">
     <el-input v-model="filterText" placeholder="输入关键字进行过滤" />
 
-    <el-tree
-      ref="tree"
-      class="filter-tree"
-      :data="data"
-      :props="defaultProps"
-      default-expand-all
-      :filter-node-method="filterNode"
-    />
+    <el-tree ref="tree" class="filter-tree" :data="data" :props="defaultProps" default-expand-all :filter-node-method="filterNode" />
   </div>
 </template>
 
 <script>
+import subject from '@/api/edu/subject'
 export default {
-
   data() {
     return {
       filterText: '',
-      data: [{
-        id: 1,
-        label: '一级 1',
-        children: [{
-          id: 4,
-          label: '二级 1-1',
-          children: [{
-            id: 9,
-            label: '三级 1-1-1'
-          }, {
-            id: 10,
-            label: '三级 1-1-2'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: '一级 2',
-        children: [{
-          id: 5,
-          label: '二级 2-1'
-        }, {
-          id: 6,
-          label: '二级 2-2'
-        }]
-      }, {
-        id: 3,
-        label: '一级 3',
-        children: [{
-          id: 7,
-          label: '二级 3-1'
-        }, {
-          id: 8,
-          label: '二级 3-2'
-        }]
-      }],
+      data: [],
       defaultProps: {
         children: 'children',
-        label: 'label'
+        label: 'title'
       }
     }
   },
@@ -66,10 +25,20 @@ export default {
     }
   },
 
+  created() {
+    this.getSubjectTree()
+  },
   methods: {
+    async getSubjectTree() {
+      const {
+        data
+      } = await subject.getSubjectTreeApi()
+      this.data = data
+    },
+
     filterNode(value, data) {
       if (!value) return true
-      return data.label.indexOf(value) !== -1
+      return data.title.indexOf(value) !== -1
     }
   }
 }
